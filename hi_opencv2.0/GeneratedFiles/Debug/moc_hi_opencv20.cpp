dd7a0e4f2827e4a673d7a404a6996022857e58fb
羊羊 <1764350797@qq.com>
2019-10-08 16:22:54 +0800

    界面

--- hi_opencv2.0/GeneratedFiles/Debug/moc_hi_opencv20.cpp
+++ hi_opencv2.0/GeneratedFiles/Debug/moc_hi_opencv20.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_hi_opencv20_t {
-    QByteArrayData data[21];
-    char stringdata0[194];
+    QByteArrayData data[27];
+    char stringdata0[269];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -51,7 +51,13 @@ QT_MOC_LITERAL(16, 154, 8), // "on_scale"
 QT_MOC_LITERAL(17, 163, 9), // "on_rotate"
 QT_MOC_LITERAL(18, 173, 6), // "on_L2R"
 QT_MOC_LITERAL(19, 180, 6), // "on_T2D"
-QT_MOC_LITERAL(20, 187, 6) // "on_pyr"
+QT_MOC_LITERAL(20, 187, 6), // "on_pyr"
+QT_MOC_LITERAL(21, 194, 8), // "on_Sobel"
+QT_MOC_LITERAL(22, 203, 10), // "on_Laplace"
+QT_MOC_LITERAL(23, 214, 8), // "on_Canny"
+QT_MOC_LITERAL(24, 223, 13), // "on_HoughLines"
+QT_MOC_LITERAL(25, 237, 15), // "on_HoughCircles"
+QT_MOC_LITERAL(26, 253, 15) // "on_findContours"
 
     },
     "hi_opencv20\0open\0\0on_blur\0on_gaussian\0"
@@ -59,7 +65,9 @@ QT_MOC_LITERAL(20, 187, 6) // "on_pyr"
     "on_slider_2\0on_erode\0on_dilate\0"
     "on_opening\0on_closing\0on_MG\0on_blackhat\0"
     "on_tophat\0on_scale\0on_rotate\0on_L2R\0"
-    "on_T2D\0on_pyr"
+    "on_T2D\0on_pyr\0on_Sobel\0on_Laplace\0"
+    "on_Canny\0on_HoughLines\0on_HoughCircles\0"
+    "on_findContours"
 };
 #undef QT_MOC_LITERAL
 
@@ -69,7 +77,7 @@ static const uint qt_meta_data_hi_opencv20[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      19,   14, // methods
+      25,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -77,27 +85,39 @@ static const uint qt_meta_data_hi_opencv20[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  109,    2, 0x08 /* Private */,
-       3,    0,  110,    2, 0x08 /* Private */,
-       4,    0,  111,    2, 0x08 /* Private */,
-       5,    0,  112,    2, 0x08 /* Private */,
-       6,    0,  113,    2, 0x08 /* Private */,
-       7,    0,  114,    2, 0x08 /* Private */,
-       8,    0,  115,    2, 0x08 /* Private */,
-       9,    0,  116,    2, 0x08 /* Private */,
-      10,    0,  117,    2, 0x08 /* Private */,
-      11,    0,  118,    2, 0x08 /* Private */,
-      12,    0,  119,    2, 0x08 /* Private */,
-      13,    0,  120,    2, 0x08 /* Private */,
-      14,    0,  121,    2, 0x08 /* Private */,
-      15,    0,  122,    2, 0x08 /* Private */,
-      16,    0,  123,    2, 0x08 /* Private */,
-      17,    0,  124,    2, 0x08 /* Private */,
-      18,    0,  125,    2, 0x08 /* Private */,
-      19,    0,  126,    2, 0x08 /* Private */,
-      20,    0,  127,    2, 0x08 /* Private */,
+       1,    0,  139,    2, 0x08 /* Private */,
+       3,    0,  140,    2, 0x08 /* Private */,
+       4,    0,  141,    2, 0x08 /* Private */,
+       5,    0,  142,    2, 0x08 /* Private */,
+       6,    0,  143,    2, 0x08 /* Private */,
+       7,    0,  144,    2, 0x08 /* Private */,
+       8,    0,  145,    2, 0x08 /* Private */,
+       9,    0,  146,    2, 0x08 /* Private */,
+      10,    0,  147,    2, 0x08 /* Private */,
+      11,    0,  148,    2, 0x08 /* Private */,
+      12,    0,  149,    2, 0x08 /* Private */,
+      13,    0,  150,    2, 0x08 /* Private */,
+      14,    0,  151,    2, 0x08 /* Private */,
+      15,    0,  152,    2, 0x08 /* Private */,
+      16,    0,  153,    2, 0x08 /* Private */,
+      17,    0,  154,    2, 0x08 /* Private */,
+      18,    0,  155,    2, 0x08 /* Private */,
+      19,    0,  156,    2, 0x08 /* Private */,
+      20,    0,  157,    2, 0x08 /* Private */,
+      21,    0,  158,    2, 0x08 /* Private */,
+      22,    0,  159,    2, 0x08 /* Private */,
+      23,    0,  160,    2, 0x08 /* Private */,
+      24,    0,  161,    2, 0x08 /* Private */,
+      25,    0,  162,    2, 0x08 /* Private */,
+      26,    0,  163,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -146,6 +166,12 @@ void hi_opencv20::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         case 16: _t->on_L2R(); break;
         case 17: _t->on_T2D(); break;
         case 18: _t->on_pyr(); break;
+        case 19: _t->on_Sobel(); break;
+        case 20: _t->on_Laplace(); break;
+        case 21: _t->on_Canny(); break;
+        case 22: _t->on_HoughLines(); break;
+        case 23: _t->on_HoughCircles(); break;
+        case 24: _t->on_findContours(); break;
         default: ;
         }
     }
@@ -177,13 +203,13 @@ int hi_opencv20::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 19)
+        if (_id < 25)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 19;
+        _id -= 25;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 19)
+        if (_id < 25)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 19;
+        _id -= 25;
     }
     return _id;
 }
