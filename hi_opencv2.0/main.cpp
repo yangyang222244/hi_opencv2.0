b005769f855e81c203aa5de7c7a24eae9bb4bbd1
w <w@W-PC>
2019-10-16 19:01:23 +0800

    相对路径

--- hi_opencv2.0/main.cpp
+++ hi_opencv2.0/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 	hi_opencv20 w;
 	w.show();
-	QFile styleFile("F:/project/hi_opencv2.0/hi_opencv2.0/Resources/stylesheet.qss");
+	QFile styleFile("./Resources/stylesheet.qss");
 	styleFile.open(QIODevice::ReadOnly);
 	QString setStyleSheet(styleFile.readAll());;
 	a.setStyleSheet(setStyleSheet);
